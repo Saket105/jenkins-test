8b3e1d8eee188dfda9abe894bc204f4d2b908b20
saket105 <saketbsc123@gmail.com>
2025-09-05 22:57:08 +0530

    Testing

---
+++ Test.java
@@ -0,0 +1,9 @@
+import java.util.Date;
+
+public class Test{
+    public static void main(String[] args) {
+        System.out.println("This will run from jenkins");
+        System.out.println("Deply in jenkins");
+        System.out.println(new Date());
+    }
+}
